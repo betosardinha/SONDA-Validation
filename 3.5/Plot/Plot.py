c4e1e7adf03d00a88264d1832ef098871a264c8e
Alberto Sardinha <sardinhabeto55@gmail.com>
2019-10-03 08:53:24 -0300

    Delete Plot.py

--- 3.5/Plot/Plot.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-def clearSkyPlot(dataArray, codeArray, clearskyArray):
-    for i in range(len(clearskyArray)):
-        if not np.isnan(clearskyArray[i][0]):
-            plt.plot(clearskyArray[i][0])
-    plt.show()
